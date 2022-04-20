be199c234d68b71a70c18270e7461be612916979
unknown <sskk8226@gmail.com>
2022-04-20 14:05:20 +0500

    done

--- src/components/Admin/Pastpapers.js
+++ src/components/Admin/Pastpapers.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef, useContext } from 'react'
 import globalContext from "../../context/globalContext"
+import Select from 'react-select';
 import classes from "./SideBar.module.css"
 
 const Pastpapers = () => {
@@ -16,54 +17,49 @@ const Pastpapers = () => {
         }
     }
 
-    const { questions, initialQuest, setInitialQuest, setQuestions, page, setPage, index, sections, setSections } = useContext(globalContext)
+    const { questions, initialQuest, setInitialQuest, setQuestions, page, setPage, index } = useContext(globalContext)
 
     const [subQuest, setSubQuest] = useState([])
+    const [options, setOptions] = useState([])
     const [subFurther, setSubFurther] = useState([])
-    const [section, setSection] = useState({name:"", type: "", marks:"", section:true})
+    const [section, setSection] = useState({ name: "", type: "", marks: "", section: true })
 
     const secRef = useRef(null)
+
+    const sections = [
+        { value: 'Section-A', label: 'Section-A', name: "section" },
+        { value: 'Section-B', label: 'Section-B', name: "section" },
+        { value: 'Section-C', label: 'Section-C', name: "section" },
+    ];
+
+    const boards = [
+        { value: 'cambridge', label: 'Cambridge', name: "board" },
+        { value: 'karachi', label: 'Karachi', name: "board" },
+        { value: 'federal', label: 'Federal', name: "board" },
+    ];
+
+    const subjects = [
+        { value: 'isl', label: 'Islamiat', name: "subject" },
+        { value: 'english', label: 'English', name: "subject" },
+        { value: 'math', label: 'Math', name: "subject" },
+        { value: 'pst', label: 'PST', name: "subject" },
+        { value: 'urdu', label: 'Urdu', name: "subject" },
+        { value: 'chem', label: 'Chemistry', name: "subject" },
+        { value: 'phy', label: 'Physics', name: "subject" },
+    ];
 
     const onChange = (e) => {
         setInitialQuest({ ...initialQuest, [e.target.name]: e.target.value })
     }
+    
     const onChangeSec = (e) => {
         setSection({ ...section, [e.target.name]: e.target.value })
     }
+
     const onChangeSub = (e) => {
         let subparts = [...subQuest];
         subparts[e.target.dataset.id][e.target.name] = e.target.value;
         setSubQuest(subparts)
-    }
-    const onChangeFurth = (e) => {
-        let subFur = [...subFurther]
-        console.log("SUBFURTHER", subFur, "SUBQUEST", subQuest)
-        for (let j = 0; j < subFurther.length; j++) {
-            if (subFurther[j]["parent"] == e.target.dataset.parent) {
-                subFur[j].finalFurther[e.target.dataset.id][e.target.name] = e.target.value
-            }
-        }
-        setSubFurther(subFur)
-    }
-    const addFurther = (i) => {
-        let checkArray = [...subFurther]
-        let parentPresent = false
-        for (let j = 0; j < checkArray.length; j++) {
-            if (checkArray[j]["parent"] == i) {
-                checkArray[j]["finalFurther"].push({ "parent": i, "qno": "", "question": "" })
-                parentPresent = true
-                break
-            } else {
-                parentPresent = false
-            }
-        };
-        let modifiedArray = [...subQuest]
-        if (checkArray.length == 0 || !parentPresent) {
-            setSubFurther(subFurther => [...subFurther, { "parent": i, "finalFurther": [{ "parent": 0, "qno": "", "question": "" }] }])
-        }
-        setSubQuest(modifiedArray)
-        console.log(subFurther)
-
     }
 
     const addSubPart = () => {
@@ -121,16 +117,7 @@ const Pastpapers = () => {
         }
     }
 
-    const addSection = (e) => {
-        secRef.current.click()
-    }
-
-    const finalSection = () => {
-        let initialSections = sections
-        initialSections.push(section)
-        setSections(initialSections)
-        console.log(sections)
-    }
+    const [typePaper, settypePaper] = useState("subjective")
 
     return (
         <div>
@@ -140,44 +127,64 @@ const Pastpapers = () => {
                 </ul>
             </div>
             <div className="min-vh-100 ">
-                <div className={`${classes.bg_Container} container`}>
-                    {/* Section Modal */}
-                    <button ref={secRef} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#sectionModal">
-                        Launch static backdrop modal
-                    </button>
-
-                    <div className="modal fade" id="sectionModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex={-1} aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title" id="staticBackdropLabel">Add Section</h5>
-                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-                                </div>
-                                <div className="modal-body">
-                                    <form>
-                                        <div className="mb-3">
-                                            <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-                                            <input type="text" className="form-control" id="fee_name" placeholder='Section A' name='name' required onChange={onChangeSec} />
-                                        </div>
-                                        <div className="mb-3">
-                                            <label htmlFor="exampleInputEmail1" className="form-label">Type</label>
-                                            <input type="text" className="form-control" id="fee_amount" placeholder='Short Question' name='type' required onChange={onChangeSec} />
-                                        </div>
-                                        <div className="mb-3">
-                                            <label htmlFor="exampleInputEmail1" className="form-label">Marks</label>
-                                            <input type="text" className="form-control" id="fee_amount" placeholder='Total marks' name='marks' required onChange={onChangeSec} />
-                                        </div>
-                                    </form>
-                                </div>
-                                <div className="modal-footer">
-                                    <button type="button" className="btn btn-outline-secondary" data-bs-dismiss="modal">Back</button>
-                                    <button type="button" className="btn btn-outline-success" data-bs-dismiss="modal" onClick={finalSection}><i className="fas fa-plus-circle mr-1"></i> Section</button>
-                                </div>
+                <div className={`container`}>
+                    <div className="d-flex flex-row justify-content-between pt-3
+                    ">
+                        <h1>Add Past Papers</h1>
+                        <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                            <input type="radio" className="btn-check" name="btnradio" id="btnradio2" autoComplete="off" defaultChecked onClick={() => settypePaper("subjective")} />
+                            <label className="btn btn-outline-success" htmlFor="btnradio2">Subjective</label>
+                            <input type="radio" className="btn-check" name="btnradio" id="btnradio3" autoComplete="off" onClick={() => settypePaper("objective")} />
+                            <label className="btn btn-outline-success" htmlFor="btnradio3">Objective</label>
+                        </div>
+                    </div>
+                    <div className="row py-4">
+                        <div className="col-md-3">
+                            <div>
+                                <Select
+                                    defaultValue={null}
+                                    onChange={onChange}
+                                    options={boards}
+                                    width={100}
+                                    placeholder="Select Board"
+                                    name="board"
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div>
+                                <Select
+                                    defaultValue={null}
+                                    onChange={onChange}
+                                    options={subjects}
+                                    width={100}
+                                    placeholder="Select Subject"
+                                    name="subject"
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div>
+                                <Select
+                                    defaultValue={null}
+                                    onChange={onChange}
+                                    options={sections}
+                                    width={100}
+                                    name="section"
+                                    placeholder="Select Section"
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <div>
+                                <input type="text" className="form-control" placeholder='Enter Year' />
                             </div>
                         </div>
                     </div>
-                    {/* Section Modal */}
-                    <div className="p-4 question_maker_border">
+                    <div className='pb-3 text-end'>
+                        <button className="btn btn-outline-success"><i class="far fa-eye me-2"></i> Preview</button>
+                    </div>
+                    {typePaper === "subjective" ? <div className="p-4 question_maker_border">
                         <div className="d-flex flex-row justify-content-between mb-3">
                             <div className='pr-4'>
                                 <label htmlFor="exampleInputPassword1" className="form-label">Question No.</label>
@@ -191,7 +198,7 @@ const Pastpapers = () => {
                         <label htmlFor="exampleInputEmail1" className="form-label">Question</label>
                         <textarea className="form-control" name='question' onKeyDown={handlekeyDown} id="question" value={initialQuest.question} onChange={onChange} placeholder="Enter your question here..." rows="6"></textarea>
                         {subQuest.map((part, i) => {
-                            return (part != "x") ?
+                            return (part != "x") ? 
                                 <div className='mt-5 mb-2 p-3 question_maker_border' key={i}>
                                     <div className='row pb-3'>
                                         <div className="col-md-4">
@@ -228,7 +235,6 @@ const Pastpapers = () => {
                         })}
                         <div className='row g-0 pt-2'>
                             <div className="col-xl-4 col-lg-12 col-md-12 col-sm-4 mediaResponsive1 pt-3">
-                                <button className="btn btn-success me-2" onClick={addSection}><i className="fas fa-plus-circle mr-1"></i> Section</button>
                                 <button className="btn btn-success" onClick={addSubPart}><i className="fas fa-plus-circle mr-1"></i> Sub-Part</button>
                             </div>
                             <div className="col-xl-8 col-lg-12 col-md-12 col-sm-8 mediaResponsive2 pt-3">
@@ -238,7 +244,48 @@ const Pastpapers = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> :
+                        <div className="p-4 question_maker_border">
+                            <div className="d-flex flex-row justify-content-between mb-3">
+                                <div className='pr-4'>
+                                    <label htmlFor="exampleInputPassword1" className="form-label">MCQ No.</label>
+                                    <input type="text" className="form-control" id="exampleInputPassword1" name='qno' onChange={onChange} />
+                                </div>
+                                <div className='pl-4'>
+                                    <label htmlFor="exampleInputPassword1" className="form-label">Marks</label>
+                                    <input type="text" className="form-control" id="exampleInputPassword1" name='marks' onChange={onChange} />
+                                </div>
+                            </div>
+                            <label htmlFor="exampleInputEmail1" className="form-label">MCQ</label>
+                            <textarea className="form-control" name='question' onKeyDown={handlekeyDown} id="question" value={initialQuest.question} onChange={onChange} placeholder="Enter your question here..." rows="6"></textarea>
+                            {options.map((part, i) => {
+                                return (part != "x") ?
+                                    <div className='mt-5 mb-2 p-3 question_maker_border' key={i}>
+                                        <div className='row pb-3'>
+                                            <div className="col-md-4">
+                                                <label htmlFor="exampleInputPassword1" className="form-label">Question No.</label>
+                                                <input type="text" className="form-control" data-id={i} id="exampleInputPassword1" name='qno' onChange={onChangeSub} />
+                                            </div>
+                                        </div>
+                                        <textarea className="form-control" name='question' data-id={i} placeholder={`Enter your question here...`} rows="4" onChange={onChangeSub}></textarea>
+                                        <div className='text-end pt-3'>
+                                            <button className="btn btn-outline-danger mr-2" onClick={() => deletePart(i)}><i className="fas fa-trash mr-2"></i>Sub-Part</button>
+                                        </div>
+                                    </div>
+                                    : ""
+                            })}
+                            <div className='row g-0 pt-2'>
+                                <div className="col-xl-4 col-lg-12 col-md-12 col-sm-4 mediaResponsive1 pt-3">
+                                    <button className="btn btn-success" onClick={addSubPart}><i className="fas fa-plus-circle mr-1"></i> Sub-Part</button>
+                                </div>
+                                <div className="col-xl-8 col-lg-12 col-md-12 col-sm-8 mediaResponsive2 pt-3">
+                                    <div className='lolResponsive'>
+                                        <button className="btn btn-danger queBtnResp" onClick={delQuestion}><i className="fas fa-trash mr-2"></i> Question</button>
+                                        <button className="btn btn-success ml-2 queBtnResp2" onClick={addQuestion}><i className="fas fa-plus-circle mr-1"></i> Question</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>}
                 </div>
             </div>
         </div>
